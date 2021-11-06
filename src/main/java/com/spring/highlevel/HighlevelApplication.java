ecd637a7d3be0008a41f9fe7c05b513908f12ef5
seokhun <n2tjrgns@gmail.com>
2021-11-06 09:51:40 +0900

    Initial commit

---
+++ src/main/java/com/spring/highlevel/HighlevelApplication.java
@@ -0,0 +1,13 @@
+package com.spring.highlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighlevelApplication.class, args);
+	}
+
+}
