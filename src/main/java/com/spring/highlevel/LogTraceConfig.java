3d0f926d8451ffb62b6878d206305cac86f0cbc3
seokhun <n2tjrgns@gmail.com>
2021-11-12 16:21:53 +0900

    ocp를 위반하지 않고 변경된 LogTrace 주입

--- src/main/java/com/spring/highlevel/LogTraceConfig.java
+++ src/main/java/com/spring/highlevel/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.spring.highlevel;
 
-import com.spring.highlevel.trace.logtrace.FieldLogTrace;
 import com.spring.highlevel.trace.logtrace.LogTrace;
+import com.spring.highlevel.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +11,6 @@ public class LogTraceConfig {
     //빈으로 등록하면 스프링에서 싱글톤으로 관리해주는것을 잊지 말자.
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
