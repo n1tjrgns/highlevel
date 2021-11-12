e15e4e572995539f48baccdf370a98ed3fa6e942
seokhun <n2tjrgns@gmail.com>
2021-11-12 16:01:42 +0900

    쓰레드 로컬 예제

---
+++ src/test/java/com/spring/highlevel/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,43 @@
+package com.spring.highlevel.trace.threadlocal;
+
+import com.spring.highlevel.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userb");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        //sleep(2000); //2초를 쉬기 때문에 동시성 문제 x
+        sleep(100); //동시성 발생 해야하지만 쓰레드 로컬을 사용했기 때문에 동시성 발생 x
+        threadB.start();
+
+        sleep(3000); // 메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
